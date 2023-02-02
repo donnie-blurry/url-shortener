cf0eeb3c69c2f28f86b02fffa404a728e9379fbf
Donnie <danialsheikhani@gmail.com>
2023-02-02 13:03:10 +0330

    Add encode route with dummy responses

--- index.js
+++ index.js
@@ -13,46 +13,82 @@ function getUrlById(shortUrlId) {
     };
 }
 
-app.get('/decode', (req, res) => {
-    const {encodedUrl} = req.query;
-    if (!encodedUrl) {
+function shorten(url) {
+    return {
+        id: "QAZWSX",
+        shortUrl: "http://short.link/QAZWSX",
+        originalUrl: url,
+        createdAt: Date.now(),
+    };
+}
+
+app.use(express.json())
+    .post('/encode', (req, res) => {
+        const {url} = req.body;
+        if (!url) {
+            return res
+                .status(400)
+                .json({
+                    message: "Should provide a url to encode"
+                });
+        }
+        if (!validator.isURL(url)) {
+            return res
+                .status(400) // TODO what is the true status code?
+                .json({
+                    message: "Should provide a valid url to encode"
+                });
+        }
+        let protocolIncludedUrl = url;
+        if (!/^https?:\/\//i.test(url)) {
+            protocolIncludedUrl = 'http://' + url;
+        }
+        const shortUrl = shorten(protocolIncludedUrl);
         return res
-            .status(400) // TODO what is the true status code?
-            .json({
-                message: "Should provide an encodedUrl"
-            });
-    }
-    if (!validator.isURL(encodedUrl)) {
+            .status(200)
+            .json(shortUrl);
+    })
+    .get('/decode', (req, res) => {
+        const {encodedUrl} = req.query;
+        if (!encodedUrl) {
+            return res
+                .status(400) // TODO what is the true status code?
+                .json({
+                    message: "Should provide a url to decode"
+                });
+        }
+        if (!validator.isURL(encodedUrl)) {
+            return res
+                .status(400) // TODO what is the true status code?
+                .json({
+                    message: "Should provide a valid url"
+                });
+        }
+        let protocolIncludedEncodedUrl = encodedUrl;
+        if (!/^https?:\/\//i.test(encodedUrl)) {
+            protocolIncludedEncodedUrl = 'http://' + encodedUrl;
+        }
+        const url = new URL(protocolIncludedEncodedUrl);
+        if (url.host !== 'short.link') {
+            return res
+                .status(400) // TODO what is the true status code?
+                .json({
+                    message: "Should provide an short.link URL."
+                });
+        }
+        const shortUrlId = url.pathname.replace(/^\/+/g, '');
+        const storedUrl = getUrlById(shortUrlId);
+        if (!storedUrl) {
+            return res
+                .status(404)
+                .json({
+                    message: "Provided short.link URL does not exist."
+                });
+        }
         return res
-            .status(400) // TODO what is the true status code?
-            .json({
-                message: "Should provide an valid encodedUrl"
-            });
-    }
-    let protocolIncludedEncodedUrl = encodedUrl;
-    if (!/^https?:\/\//i.test(encodedUrl)) {
-        protocolIncludedEncodedUrl = 'http://' + encodedUrl;
-    }
-    const url = new URL(protocolIncludedEncodedUrl);
-    if (url.host !== 'short.link') {
-        return res
-            .status(400) // TODO what is the true status code?
-            .json({
-                message: "Should provide an short.link URL."
-            });
-    }
-    const shortUrlId = url.pathname.replace(/^\/+/g, '');
-    const storedUrl = getUrlById(shortUrlId);
-    if (!storedUrl) {
-        return res
-            .status(404)
-            .json({
-                message: "Provided short.link URL does not exist."
-            });
-    }
-    return res
-        .status(200)
-        .json(storedUrl);
-}).listen(PORT, () =>
-    console.log(`Example app listening on port ${PORT}!`),
-);
+            .status(200)
+            .json(storedUrl);
+    })
+    .listen(PORT, () =>
+        console.log(`Example app listening on port ${PORT}!`),
+    );
