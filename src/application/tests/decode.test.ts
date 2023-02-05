5253da328bd9126de10fc95eb005c4e4dc28b57d
Donnie <danialsheikhani@gmail.com>
2023-02-05 18:50:30 +0330

    Add test for decode app

--- src/application/tests/decode.test.ts
+++ src/application/tests/decode.test.ts
@@ -1,8 +1,16 @@
 import decodeApp from '../decode';
-
+import dataAccess from '../../data-access';
+import {jest} from '@jest/globals';
 describe('Decode application unit tests', () => {
   it('should respond with an error if encoded url is not short.link', async () => {
     const encodedUrl = 'example.com';
     expect(() => decodeApp.decode(encodedUrl)).toThrow('Should provide a short.link URL.');
+  });
+  
+  it('should parse url and get pathname as short url id', () => {
+    const url = 'https://www.short.link/ABCDEF';
+    jest.spyOn(dataAccess, 'getById');
+    decodeApp.decode(url);
+    expect(dataAccess.getById).toBeCalledWith('ABCDEF');
   });
 });
